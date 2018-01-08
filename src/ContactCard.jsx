45f17d169ace3a7a494bed3bb0a7527791b69f5a
Manjunath <manjunath.mt0@gmail.com>
2018-01-09 00:35:27 +0530

    Fixes

--- src/ContactCard.jsx
+++ src/ContactCard.jsx
@@ -6,13 +6,13 @@ const ContactCard = ({contact}) => {
 
 	  	<div>
 	        <div className="col-xs-4 col-sm-3">
-	            <img src={contact.photo} alt="Scott Stevens" className="img-responsive img-circle" />
+	            <img src={contact.photo} alt={contact.name + ' ' + contact.surname} className="img-responsive img-circle" />
 	        </div>
 	        <div className="col-xs-8 col-sm-9">
 	            <span className="name">{contact.name + ' ' + contact.surname}</span><br/>
 	            
-	            <span className="fa fa-map-marker text-muted c-info" title="5842 Hillcrest Rd"></span>
-	            <span className="visible-xs"> <span className="text-muted">5842 Hillcrest Rd</span><br/></span>
+	            <span className="fa fa-map-marker text-muted c-info" title={contact.address}></span>
+	            <span className="visible-xs"> <span className="text-muted">{contact.address}</span><br/></span>
 	            
 	            <span className="fa fa-microphone text-muted c-info" title={contact.phone}></span>
 	            <span className="visible-xs"> <span className="text-muted">{contact.phone}</span><br/></span>
