1cb059ede970dd36037870ee363dfb29099ea76e
Manjunath M <manjunath.mt0@gmail.com>
2018-05-18 11:41:10 +0530

    v1 complete

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
 import React from 'react';
-import {render}from 'react-dom';
+import {render} from 'react-dom';
 import App from './App';
-import configureStore  from './store/configureStore';
-import {toggleContactForm,
-	handleInputChange} from './actions';
+
 render(
     <App />,
   document.getElementById('root')
 )
-
-const store = configureStore();
-
- store.subscribe(() =>
-  console.log(store.getState())
-)
-
-/* returns isContactFormHidden returns false */
-store.dispatch(toggleContactForm());
-/* returns isContactFormHidden returns false */
-store.dispatch(toggleContactForm());
-store.dispatch(handleInputChange('email', 'manjunath@redmonark.com'))
 
--- src/store/configureStore.js
+++
@@ -1,9 +0,0 @@
-import {createStore} from 'redux';  
-import rootReducer from '../reducers/';  
-
-/*Create a function called configureStore */
-
-export default function configureStore() {  
-  return createStore(rootReducer);
-}
-
